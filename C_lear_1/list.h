b5dab5a183031e27e9ac6f8720eba133c9d24bab
fengbing <1305913601@qq.com>
2023-07-22 23:18:09 +0800

    提交练习

---
+++ C_lear_1/list.h
@@ -0,0 +1,29 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+struct Node;
+typedef struct Node *PtrtoNode;
+typedef PtrtoNode Position;
+typedef PtrtoNode List;
+
+List CreatHeadNode(void); // 创建头结点
+void InsertForHead(int x, List L); // 在头结点插入数据
+void InsertForLast(int x, List L); // 在头结点插入数据
+void PrintfForList(List L); // 在头结点插入数据
+int FindKthForList(List L, int pos); // 打印第pos个元素
+void DeleteKthForList(List L, int pos); // 删除第pos个元素
+void DeleteEleForList(List L, int x); // 删除值是x的元素
+//
+List MakeEmpty(List L);                 // 清空链表
+bool IsEmpty(List L);                    // 当前列表是否为空
+bool IsLast(Position P, List L);         // 判断是否是最后一个元素
+Position Find(int x, List L);           // 判断元素是否在列表中
+Position FindKth(int pos, List L);        // 查找该位置上的元素
+void Delete(int x, List L);             // 删除某元素
+Position FindPrevious(int x, List L);   // 找到元素的前一个地址
+void Insert(int x, List L, Position P); // 插入元素
+void DeleteList(List L);                // 删除表
+Position Header(List L);                // 头结点
+Position Frist(List L);                 //
+
+#endif
