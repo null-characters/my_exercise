8786279d47dfa7c7d24159fb26f670933f57a052
fengbing <1305913601@qq.com>
2024-01-25 01:45:06 +0800

    交换数据测试

---
+++ lenxi/swap.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/// @brief 使用异或的方法交换两个值
+/// @param a
+/// @param b
+void swap(int* a, int* b)
+{
+    // 根据异或满足交换律和结合律 -- 交换律: a ^ b = b ^ a  ; 结合律: a ^ b ^ c = a ^ (b ^ c)
+    *a = *a ^ *b; // a = a ^ b, b = b;
+    *b = *a ^ *b; // b = a ^ b ^ b = a ; a = a ^ b;
+    *a = *a ^ *b; // a = a ^ b ^ a = b;
+    printf_s("a=%d, b=%d\n", a, b);
+}
+
+void main(void)
+{
+    int a, b;
+    int *pt_a, *pt_b;
+    pt_a = &a;
+    pt_b = &b;
+    a = 15;
+    b = 23;
+    printf_s("a=%d, b=%d\n", a, b);
+    swap(pt_a, pt_b);
+    printf_s("a=%d, b=%d\n", a, b);
+
+    // printf_s("hello, ");
+    getchar();
+}
---
+++ lenxi/swap.s
@@ -0,0 +1,94 @@
+	.file	"swap.c"
+	.text
+	.section .rdata,"dr"
+.LC0:
+	.ascii "a=%d, b=%d\12\0"
+	.text
+	.globl	swap
+	.def	swap;	.scl	2;	.type	32;	.endef
+	.seh_proc	swap
+swap:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movq	%rcx, 16(%rbp)
+	movq	%rdx, 24(%rbp)
+	movq	16(%rbp), %rax
+	movl	(%rax), %edx
+	movq	24(%rbp), %rax
+	movl	(%rax), %eax
+	xorl	%eax, %edx
+	movq	16(%rbp), %rax
+	movl	%edx, (%rax)
+	movq	16(%rbp), %rax
+	movl	(%rax), %edx
+	movq	24(%rbp), %rax
+	movl	(%rax), %eax
+	xorl	%eax, %edx
+	movq	24(%rbp), %rax
+	movl	%edx, (%rax)
+	movq	16(%rbp), %rax
+	movl	(%rax), %edx
+	movq	24(%rbp), %rax
+	movl	(%rax), %eax
+	xorl	%eax, %edx
+	movq	16(%rbp), %rax
+	movl	%edx, (%rax)
+	movq	24(%rbp), %rax
+	movq	%rax, %r8
+	movq	16(%rbp), %rdx
+	leaq	.LC0(%rip), %rcx
+	call	printf_s
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$64, %rsp
+	.seh_stackalloc	64
+	.seh_endprologue
+	call	__main
+	leaq	-20(%rbp), %rax
+	movq	%rax, -8(%rbp)
+	leaq	-24(%rbp), %rax
+	movq	%rax, -16(%rbp)
+	movl	$15, -20(%rbp)
+	movl	$23, -24(%rbp)
+	movl	-24(%rbp), %edx
+	movl	-20(%rbp), %eax
+	movl	%edx, %r8d
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rcx
+	call	printf_s
+	movq	-16(%rbp), %rdx
+	movq	-8(%rbp), %rax
+	movq	%rax, %rcx
+	call	swap
+	movl	-24(%rbp), %edx
+	movl	-20(%rbp), %eax
+	movl	%edx, %r8d
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rcx
+	call	printf_s
+	call	getchar
+	nop
+	addq	$64, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-win32-sjlj-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	printf_s;	.scl	2;	.type	32;	.endef
+	.def	getchar;	.scl	2;	.type	32;	.endef
