3ba94b8b29a514eb2fdbece54dcb659bd4152189
fengbing <1305913601@qq.com>
2024-02-04 23:28:11 +0800

    【只出现一次的数字】算法题

---
+++ algo/eor_2.s
@@ -0,0 +1,323 @@
+	.file	"eor_lear.c"
+	.text
+	.globl	arr_test
+	.data
+	.align 32
+arr_test:
+	.long	0
+	.long	0
+	.long	1
+	.long	6
+	.long	6
+	.long	8
+	.long	1
+	.long	8
+	.long	8
+	.long	0
+	.long	0
+	.long	9
+	.globl	arr_test_return
+	.bss
+	.align 4
+arr_test_return:
+	.space 1
+	.section .rdata,"dr"
+.LC0:
+	.ascii "no charter\12\0"
+.LC1:
+	.ascii "%d\12\0"
+	.text
+	.globl	xor_array_4_one
+	.def	xor_array_4_one;	.scl	2;	.type	32;	.endef
+	.seh_proc	xor_array_4_one
+xor_array_4_one:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	movq	%rcx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	movl	$0, -4(%rbp)
+	cmpl	$0, 24(%rbp)
+	jne	.L2
+	leaq	.LC0(%rip), %rcx
+	call	printf_s
+	jmp	.L1
+.L2:
+	cmpl	$1, 24(%rbp)
+	jne	.L4
+	movq	16(%rbp), %rax
+	movl	(%rax), %eax
+	movl	%eax, %edx
+	leaq	.LC1(%rip), %rcx
+	call	printf_s
+	jmp	.L1
+.L4:
+	movl	$0, -8(%rbp)
+	jmp	.L5
+.L6:
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	16(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	xorl	%eax, -4(%rbp)
+	addl	$1, -8(%rbp)
+.L5:
+	movl	-8(%rbp), %eax
+	cmpl	24(%rbp), %eax
+	jl	.L6
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	leaq	.LC1(%rip), %rcx
+	call	printf_s
+	nop
+.L1:
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.globl	xor_array_4_two
+	.def	xor_array_4_two;	.scl	2;	.type	32;	.endef
+	.seh_proc	xor_array_4_two
+xor_array_4_two:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$64, %rsp
+	.seh_stackalloc	64
+	.seh_endprologue
+	movq	%rcx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	movl	$0, -4(%rbp)
+	cmpl	$0, 24(%rbp)
+	jne	.L8
+	leaq	.LC0(%rip), %rcx
+	call	printf_s
+	jmp	.L7
+.L8:
+	cmpl	$1, 24(%rbp)
+	jne	.L10
+	movq	16(%rbp), %rax
+	movl	(%rax), %eax
+	movl	%eax, %edx
+	leaq	.LC1(%rip), %rcx
+	call	printf_s
+	jmp	.L7
+.L10:
+	movl	$0, -8(%rbp)
+	jmp	.L11
+.L12:
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	16(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	xorl	%eax, -4(%rbp)
+	addl	$1, -8(%rbp)
+.L11:
+	movl	-8(%rbp), %eax
+	cmpl	24(%rbp), %eax
+	jl	.L12
+	movl	-4(%rbp), %eax
+	negl	%eax
+	andl	-4(%rbp), %eax
+	movl	%eax, -20(%rbp)
+	movl	$0, -12(%rbp)
+	movl	$0, -24(%rbp)
+	movl	$0, -16(%rbp)
+	jmp	.L13
+.L15:
+	movl	-16(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	16(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	andl	-20(%rbp), %eax
+	testl	%eax, %eax
+	je	.L14
+	movl	-16(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	16(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	xorl	%eax, -12(%rbp)
+.L14:
+	addl	$1, -16(%rbp)
+.L13:
+	movl	-16(%rbp), %eax
+	cmpl	24(%rbp), %eax
+	jl	.L15
+	movl	-4(%rbp), %eax
+	xorl	-12(%rbp), %eax
+	movl	%eax, -24(%rbp)
+.L7:
+	addq	$64, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.globl	singleNumber
+	.def	singleNumber;	.scl	2;	.type	32;	.endef
+	.seh_proc	singleNumber
+singleNumber:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$64, %rsp
+	.seh_stackalloc	64
+	.seh_endprologue
+	movq	%rcx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	movq	%r8, 32(%rbp)
+	movq	32(%rbp), %rax
+	movl	$2, (%rax)
+	movl	$8, %ecx
+	call	malloc
+	movq	%rax, 32(%rbp)
+	cmpl	$2, 24(%rbp)
+	jg	.L17
+	movq	16(%rbp), %rax
+	movl	(%rax), %edx
+	movq	32(%rbp), %rax
+	movl	%edx, (%rax)
+	movq	32(%rbp), %rax
+	addq	$4, %rax
+	movq	16(%rbp), %rdx
+	movl	4(%rdx), %edx
+	movl	%edx, (%rax)
+.L17:
+	movl	$0, -4(%rbp)
+	movl	$0, -8(%rbp)
+	jmp	.L18
+.L19:
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	16(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	xorl	%eax, -4(%rbp)
+	addl	$1, -8(%rbp)
+.L18:
+	movl	-8(%rbp), %eax
+	cmpl	24(%rbp), %eax
+	jl	.L19
+	movl	-4(%rbp), %eax
+	negl	%eax
+	andl	-4(%rbp), %eax
+	movl	%eax, -20(%rbp)
+	movl	$0, -12(%rbp)
+	movl	$0, -16(%rbp)
+	jmp	.L20
+.L22:
+	movl	-16(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	16(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	cmpl	%eax, -20(%rbp)
+	sete	%al
+	movzbl	%al, %eax
+	andl	-20(%rbp), %eax
+	testl	%eax, %eax
+	je	.L21
+	movl	-16(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	16(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	xorl	%eax, -12(%rbp)
+.L21:
+	addl	$1, -16(%rbp)
+.L20:
+	movl	-16(%rbp), %eax
+	cmpl	24(%rbp), %eax
+	jl	.L22
+	movq	32(%rbp), %rax
+	movl	-12(%rbp), %edx
+	movl	%edx, (%rax)
+	movq	32(%rbp), %rax
+	leaq	4(%rax), %rdx
+	movl	-12(%rbp), %eax
+	xorl	-4(%rbp), %eax
+	movl	%eax, (%rdx)
+	movq	32(%rbp), %rax
+	addq	$64, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC2:
+	.ascii "%d, %d\12\0"
+.LC3:
+	.ascii "\12\347\250\213\345\272\217\350\277\220\350\241\214\344\272\206%ld\345\276\256\347\247\222\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$64, %rsp
+	.seh_stackalloc	64
+	.seh_endprologue
+	call	__main
+	leaq	-16(%rbp), %rax
+	movl	$0, %edx
+	movq	%rax, %rcx
+	call	mingw_gettimeofday
+	movl	$12, -4(%rbp)
+	movl	-4(%rbp), %eax
+	leaq	arr_test_return(%rip), %r8
+	movl	%eax, %edx
+	leaq	arr_test(%rip), %rcx
+	call	singleNumber
+	movl	4+arr_test_return(%rip), %edx
+	movl	arr_test_return(%rip), %eax
+	movl	%edx, %r8d
+	movl	%eax, %edx
+	leaq	.LC2(%rip), %rcx
+	call	printf
+	leaq	-24(%rbp), %rax
+	movl	$0, %edx
+	movq	%rax, %rcx
+	call	mingw_gettimeofday
+	movl	-24(%rbp), %edx
+	movl	-16(%rbp), %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	imull	$1000000, %eax, %eax
+	movl	-20(%rbp), %ecx
+	movl	-12(%rbp), %edx
+	subl	%edx, %ecx
+	movl	%ecx, %edx
+	addl	%edx, %eax
+	movl	%eax, -8(%rbp)
+	movl	-8(%rbp), %eax
+	movl	%eax, %edx
+	leaq	.LC3(%rip), %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$64, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-win32-sjlj-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	printf_s;	.scl	2;	.type	32;	.endef
+	.def	malloc;	.scl	2;	.type	32;	.endef
+	.def	mingw_gettimeofday;	.scl	2;	.type	32;	.endef
+	.def	printf;	.scl	2;	.type	32;	.endef
---
+++ algo/eor_lear.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// 第一题,验证异或的用法
+// 题目是验证一个数组中有一种数是奇数个数,找出这个数
+
+// 题目二是验证一个数组中有两种数是奇数个数,找出这两个数
+
+int arr_test[] = {0, 0, 1, 6, 6, 8, 1, 8, 8, 0, 0, 9};
+int arr_test_return[] = {};
+// int arr_test[] = {};
+// 定义一个函数，参数是一个整型数组和它的长度
+void xor_array_4_one(int arr[], int len)
+{
+    int i = 0; // 循环用来异或的数
+    if (len == 0)
+    {
+        printf_s("no charter\n");
+        return; // 如果数组为空，直接返回
+    }
+
+    if (len == 1)
+    {
+        printf_s("%d\n", arr[0]);
+        return; // 如果数组只有一个元素，直接返回
+    }
+
+    for (int cur_i = 0; cur_i < len; cur_i++)
+    {
+        i ^= arr[cur_i]; // 对数组中的每个元素进行异或操作
+    }
+
+    printf_s("%d\n", i); // 打印异或的结果
+}
+
+void xor_array_4_two(int arr[], int len)
+{
+    int eor = 0; // 循环用来异或的数
+    if (len == 0)
+    {
+        printf_s("no charter\n");
+        return; // 如果数组为空，直接返回
+    }
+
+    if (len == 1)
+    {
+        printf_s("%d\n", arr[0]);
+        return; // 如果数组只有一个元素，直接返回
+    }
+
+    for (int cur_i = 0; cur_i < len; cur_i++)
+    {
+        eor ^= arr[cur_i]; // 对数组中的每个元素进行异或操作
+    }
+
+    int rightOne = eor & (~eor + 1); // 找出eor中最右侧一个1的位置
+    // 如果eor = 00001100,   ~eor = 11110011  ~eor+1 = 11110100   11110100 & 00001100 = 00001000 
+
+    int onlyOne = 0; // 存储其中一个奇数个数的数
+    int onlyTwo = 0; // 存储另外一个奇数个数的数
+    for (int cur_i = 0; cur_i < len; cur_i++)
+    {
+        if ((arr[cur_i] & rightOne) != 0) {
+            // 因为其已经保证了a和b在这个位上的不同, 所以只会在a和b之间选择出一个
+            onlyOne ^= arr[cur_i];
+        }
+    }
+    
+    onlyTwo = eor ^ onlyOne;
+}
+
+int* singleNumber(int* nums, int numsSize, int* returnSize) {
+    int* result = (int*)malloc(sizeof(int) * 2);
+    *returnSize = 2;
+
+    if (numsSize == 0)
+    {
+        return result;
+    }
+
+    if (numsSize == 1)
+    {
+        result[0] = nums[0];
+        result[1] = 0;
+        return result;
+    }
+
+    int eor = 0;
+    for (int cur_i = 0; cur_i < numsSize; cur_i++)
+    {
+        eor ^= nums[cur_i];
+    }
+
+    // 以下两句都是寻找最右的1，如果eor是最小边界值，第一句代码会返回 INT_MIN，而第二句代码会返回 0。
+    int right_one = (eor == INT_MIN ? eor : (eor & (~eor + 1)));
+    // int right_one = (eor == INT_MIN ? eor : eor & (-eor)); 
+
+    int eor_0 = 0;
+    for (int cur_i = 0; cur_i < numsSize; cur_i++)
+    {
+        if (right_one & nums[cur_i])
+        {   // 这里把数分成了两组，一组是右侧第一个1位置上有1的数， 另一组是该位置上没有1的数
+            // 如果该位置上有1，则将那个数异或进来，轮询完后会消掉所有偶数项，留下的就是分组位置有1的那个数
+            eor_0 ^= nums[cur_i]; 
+        }
+    }
+    result[0] = eor_0;
+    result[1] = eor_0 ^ eor;
+    return result;
+}
+
+int main(void)
+{
+    struct timeval start, end; // 定义两个变量，用来记录开始和结束的时间
+    mingw_gettimeofday(&start, NULL); // 记录程序开始的时间
+
+    int len = sizeof(arr_test) / sizeof(arr_test[0]);
+    // xor_array_4_one(arr_test, len); // 调用函数
+
+    singleNumber(arr_test, len, arr_test_return);
+    printf("%d, %d\n", arr_test_return[0], arr_test_return[1]); // 输出结果
+
+    mingw_gettimeofday(&end, NULL);                                                    // 记录程序结束的时间
+    long time = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec); // 计算程序的运行时间
+    printf("\n程序运行了%ld微秒\n", time); // 输出结果
+    return 0;
+}
