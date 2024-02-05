88411f750dcc86f3ce3fa85929e91d82f9f57314
fengbing <1305913601@qq.com>
2024-02-05 16:55:19 +0800

    优化比较条件

--- algo/select_sorting.c
+++ algo/select_sorting.c
@@ -11,30 +11,23 @@ int arr_test[] = {2, 7, 1, 9, 6};
 /// @param arr_return 输出数组
 void select_sortint_func(int *arr, int Size, int *arr_return)
 {
-    int p_range = 0; // 有序的范围标定
-
-    for (int i = 0; i < Size - 1; i++)
-    {              // 一共轮询Size - 1次
-        p_range++; // 一旦轮到最后了，有序的范围就增一，直到范围值等于Size或者Size轮询完, 该值从1开始，表示检测0-1这个范围内的大小
-        for (int j = p_range; j > 0; j--)
+    for (int i = 0; i < Size; i++)
+    {
+        // for (int j = i; j > 0; j--)
+        // {
+        //     if (arr_return[j] >= arr_return[j - 1])
+        //     {
+        //         break;
+        //     }
+        // 此处如果条件不成立直接结束循环, 比加break条件好
+        for (int j = i; j > 0 && arr_return[j] < arr_return[j - 1]; j--)
         {
-            if (arr_return[j] >= arr_return[j - 1])
-            {
-                break;
-            }
-
             // 如果前面的比后面大，就交换两个的位置 - 从小到大排序
             int temp;
             temp = arr_return[j];
             arr_return[j] = arr_return[j - 1];
             arr_return[j - 1] = temp;
         }
-
-        // for (int i = 0; i < Size; i++)
-        // {
-        //     printf_s("%d\t", arr_return[i]);
-        // }
-        // printf_s("\n");
     }
 }
 
